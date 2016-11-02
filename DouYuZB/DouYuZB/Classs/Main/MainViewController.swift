631035cf25ad99102464fd5de63ec29c286b72a9
HCL黄 <hclhuang@HCLhuangdeMacBook-Pro.local>
2016-11-03 03:11:49 +0800

    创建4个控制器作为tabBar控制器的子控制器

---
+++ DouYuZB/DouYuZB/Classs/Main/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  DouYuZB
+//
+//  Created by HCL黄 on 16/11/3.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addChildVc("Home")
+        addChildVc("Live")
+        addChildVc("Follow")
+        addChildVc("Profile")
+    }
+
+    private func addChildVc(storyName: String) {
+        // 1.通过storyboard获取控制器
+        let childVc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        
+        // 2.将childVc作为子控制器
+        addChildViewController(childVc)
+    }
+
+}
